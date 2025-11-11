b816edf8426339214896995420dae872a22d5089
Kauan Felipe Nascimento da Silva <kauan.silva@alunos.bpkedu.com.br>
2025-11-11 20:08:41 -0300

    final

--- studio-microbio/schemaTypes/index.js
+++
@@ -1,5 +0,0 @@
-import {novidades} from './novidades.js'
-import {comentarios} from './comentarios.js'
-import {servicos} from './servicos.js'
-
-export const schemaTypes = [novidades, comentarios, servicos]
